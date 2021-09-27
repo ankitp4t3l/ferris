6d87cc27106dfde8c07f95fe37fe42a10d06ae8c
ankitp4t3l <ankitp4t3l@gmail.com>
2021-09-26 19:03:47 -0700

    Make the shebang line compatible with more envs.
    Also add some debugging info to confirm the run-as USER is correct.

--- automation/ninja.sh
+++ automation/ninja.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ -z "$WORKDIR" ]]; then
 	export WORKDIR="$(pwd)";
 fi
 
+echo $USER
 docker run --user $(id -u $USER):$(id -g $USER) -e WORKDIR=$WORKDIR -v $WORKDIR:/workdir -v /var/run/docker.sock:/var/run/docker.sock -t pierrechevalier83/ferris_automation ninja $@
 
